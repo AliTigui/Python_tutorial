49735dde86afabd85201e6ccef465c5d021af5d2
AliTigui <alitighiouart2001@gmail.com>
2024-02-18 00:06:47 +0100

    added string

--- tutorial1/main.py
+++ tutorial1/main.py
@@ -21,12 +21,22 @@ print(not b1)
 print(b1 and b2)
 print(b1 or b2)
 
+# Working with String
+username="Ali abdelghani"
+name=" Anunnake"
+full_name=username+name
+print(username.title())
+print(username.upper())
+print(username.lower())
+print(username.split())
+print(username[1:-4])
+print(username[0])
+print(full_name)
+print("#"*5)
+print("ali".center(10,"_"))
+
 # Working with array
 s=set()
 s.add(4)
 d={"name":"ali","age":20}
-for i in range(4):
-    print(i)
-for j in k:
-    print(j)
-n=int(input("Enter number :"))
+
