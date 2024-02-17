c622010eb68ca4a4ed7c97aa1ba9e910326dbbbc
AliTigui <alitighiouart2001@gmail.com>
2024-02-18 00:17:42 +0100

    a finished list

--- tutorial1/main.py
+++ tutorial1/main.py
@@ -52,6 +52,10 @@ mylist.sort()
 mylist.reverse()
 list2=[1,2,3,4,5]
 print(mylist+list2)
+my2Dlist=[[1,2],[4,5],[6,7],[8,9]]
+print(len(my2Dlist))
+print(my2Dlist[0])
+print(my2Dlist[0][1])
 
 
 s=set()
