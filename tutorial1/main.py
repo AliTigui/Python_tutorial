d818281cdcdfc0179da0af910b2a7ba5e1fcdd92
AliTigui <alitighiouart2001@gmail.com>
2024-02-17 23:52:39 +0100

    working with variables

--- tutorial1/main.py
+++ tutorial1/main.py
@@ -1,12 +1,27 @@
 # getting input and printing output
-name=input("Enter your name : ")
-age=int(input("Enter your age : "))
+name = input("Enter your name : ")
+age = int(input("Enter your age : "))
 print(f"your name is {name} and you are {age} old")
-a=4
-b="hello"
-c=4.5
-g=(4,5)
-k=[4,7,9,10]
+
+# Working with float and Integer
+a = 5
+b = 2.5
+print(a+b)
+print(a-b)
+print(a*b)
+print(a*b)
+print(a**2)
+print(a//2)
+print(a%3)
+
+# Working with Booleans
+b1=True
+b2=False
+print(not b1)
+print(b1 and b2)
+print(b1 or b2)
+
+# Working with array
 s=set()
 s.add(4)
 d={"name":"ali","age":20}
