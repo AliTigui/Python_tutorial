49d168983da89208de8a2bb8ae38125b5645e291
AliTigui <alitighiouart2001@gmail.com>
2024-02-18 21:19:44 +0100

    finished working with modules

---
+++ Introduction/tutorial5/Readme.md
@@ -0,0 +1,2 @@
+## Tutorial 5
+This tutorial will be library how to use others modules and we will see ho regular expression work in Python
---
+++ Introduction/tutorial5/utility.py
@@ -0,0 +1,11 @@
+def say_hello():
+    print("hello")
+def greet_name(name):
+    print(f"hello {name}")
+
+def main():
+    say_hello()
+    greet_name("Ali")
+
+if __name__=="__main__":
+    main()
