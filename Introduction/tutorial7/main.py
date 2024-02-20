fa29c3bae6da25d8ac1d43bd98d12023c7aed4d7
AliTigui <alitighiouart2001@gmail.com>
2024-02-20 13:50:04 +0100

    finished working with class and oop

--- Introduction/tutorial7/main.py
+++ Introduction/tutorial7/main.py
@@ -35,4 +35,66 @@ mycat=Cat("catos",10)
 mycat.talk()
 print(f"{mycat.get_name()} is {mycat.get_age()} year old")
 
+# class Inheriting in python 
+class Robot():
+    def __init__(self,id,x,y):
+        self.id=id
+        self.x=x
+        self.y=y
+        self.orientation=0
+        self._step=1
+    def turn_clockWise(self):
+        self.orientation += 90
+        if self.orientation==360:
+            self.orientation=0
+    def turn_antiClockWise(self):
+        
+        self.orientation -= 90
+        if self.orientation<0:
+            self.orientation=270
+    def walk(self,):
+        if self.orientation == 0 or self.orientation % 360 == 0:
+           self.x+=self._step
+        elif self.orientation % 270 == 0:
+            self.y-=self._step
+        elif self.orientation % 180 == 0:
+            self.x-=self._step
+        elif self.orientation % 90 == 0:
+            self.y+=self._step
+    def get_position(self):
+        print(f"the robot is at ({self.x},{self.y})")
+
+myrobot=Robot("41894",0,0)
+myrobot.walk()
+myrobot.turn_clockWise()    
+myrobot.walk()
+myrobot.get_position()
+
+class Robot_G2(Robot):
+    def __init__(self,id,x,y,charge):
+        super().__init__(id,x,y)
+        self.charge=charge
+        self.turbo_state=False
+    def turbo(self):
+        if self.charge>0:
+            self.turbo_state=True
+        else :
+            print("out of energy")
+    def walk(self):
+        if self.turbo_state and self.charge > 0:
+            self._step=2
+            self.charge-=1
+        else:
+            self.turbo_state=False
+            self._step=1
+        super().walk()
+robot2gen=Robot_G2("4484",0,0,10)
+robot2gen.turbo()
+robot2gen.walk()
+robot2gen.turn_antiClockWise()
+robot2gen.walk()
+robot2gen.turn_clockWise()
+robot2gen.walk()
+robot2gen.get_position()
+
 
